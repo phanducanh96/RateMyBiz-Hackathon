aeaecc4c2852e6567213c23747136fb162409793
phanducanh96 <aphan@uvm.edu>
2022-04-09 22:52:18 -0400

    Added requirements file, decode QR png

--- back-end/issuer-qrcode.py
+++ back-end/issuer-qrcode.py
@@ -1,12 +1,20 @@
 import requests
+from pyzbar import pyzbar
+from PIL import Image
 
 def api_qr_call():
     url = "http://issuer-sandbox.circle.com/api/v1/issuance/qrcode"
     response = requests.get(url)
     if response.status_code == 200:
         print("Issuer Verified!")
-        with open("qr-code.png", 'wb') as f:
+        with open("back-end/qr-code.png", 'wb') as f:
             f.write(response.content)
+
+def read_qr_code():
+    img = Image.open("back-end/qr-code.png")
+    output = pyzbar.decode(img)
+    print(output)
 
 if __name__ == "__main__":
     api_qr_call()
+    read_qr_code()
