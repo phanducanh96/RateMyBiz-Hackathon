3367177821af5607fb7f1f3f9d92c33d9cb955fd
phanducanh96 <aphan@uvm.edu>
2022-04-15 23:14:42 -0400

    Updated contracts

--- main/back-end/smart-contracts/migrations/2_deploy_contracts.js
+++ main/back-end/smart-contracts/migrations/2_deploy_contracts.js
@@ -2,5 +2,5 @@ const ProfileRating = artifacts.require("./ProfileRating.sol");
 
 module.exports = function (deployer, network, accounts) {
     console.log(accounts[0]);
-    deployer.deploy(ProfileRating, '0x65F744Ce44082b61A03da55a2Db541508557AD5B', { from: accounts[1] });
+    deployer.deploy(ProfileRating, '0x65F744Ce44082b61A03da55a2Db541508557AD5B', { from: accounts[0] });
 };
