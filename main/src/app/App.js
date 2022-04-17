5bf4d3cd5836ca569011c6ee22f1dde3e5542819
phanducanh96 <aphan@uvm.edu>
2022-04-17 13:27:18 -0400

    Connecting React to Smart Contract

--- main/src/app/App.js
+++ main/src/app/App.js
@@ -7,26 +7,30 @@ import Sidebar from './shared/Sidebar';
 import SettingsPanel from './shared/SettingsPanel';
 // import Footer from './shared/Footer';
 import { withTranslation } from "react-i18next";
+import Web3 from 'web3';
+import { alphaNumerate } from 'chartist';
+import { PROFILE_DETAIL_ABI, PROFILE_DETAIL_ADDRESS } from '../contracts-config'
 
 class App extends Component {
   state = {}
   componentDidMount() {
     this.onRouteChanged();
+    this.loadBlockchainData();
   }
-  render () {
-    let navbarComponent = !this.state.isFullPageLayout ? <Navbar/> : '';
-    let sidebarComponent = !this.state.isFullPageLayout ? <Sidebar/> : '';
-    let SettingsPanelComponent = !this.state.isFullPageLayout ? <SettingsPanel/> : '';
+  render() {
+    let navbarComponent = !this.state.isFullPageLayout ? <Navbar /> : '';
+    let sidebarComponent = !this.state.isFullPageLayout ? <Sidebar /> : '';
+    let SettingsPanelComponent = !this.state.isFullPageLayout ? <SettingsPanel /> : '';
     // let footerComponent = !this.state.isFullPageLayout ? <Footer/> : '';
     return (
       <div className="container-scroller">
-        { navbarComponent }
+        {navbarComponent}
         <div className="container-fluid page-body-wrapper">
-          { sidebarComponent }
+          {sidebarComponent}
           <div className="main-panel">
             <div className="content-wrapper">
-              <AppRoutes/>
-              { SettingsPanelComponent }
+              <AppRoutes />
+              {SettingsPanelComponent}
             </div>
             {/* { footerComponent } */}
           </div>
@@ -45,7 +49,7 @@ class App extends Component {
     console.log("ROUTE CHANGED");
     const { i18n } = this.props;
     const body = document.querySelector('body');
-    if(this.props.location.pathname === '/layout/RtlLayout') {
+    if (this.props.location.pathname === '/layout/RtlLayout') {
       body.classList.add('rtl');
       i18n.changeLanguage('ar');
     }
@@ -55,7 +59,7 @@ class App extends Component {
     }
     window.scrollTo(0, 0);
     const fullPageLayoutRoutes = ['/login', '/signup', '/user-pages/lockscreen', '/error-pages/error-404', '/error-pages/error-500', '/general-pages/landing-page'];
-    for ( let i = 0; i < fullPageLayoutRoutes.length; i++ ) {
+    for (let i = 0; i < fullPageLayoutRoutes.length; i++) {
       if (this.props.location.pathname === fullPageLayoutRoutes[i]) {
         this.setState({
           isFullPageLayout: true
@@ -68,6 +72,45 @@ class App extends Component {
         })
         document.querySelector('.page-body-wrapper').classList.remove('full-page-wrapper');
       }
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = new Web3(Web3.givenProvider || "http://127.0.0.1:7545");
+    await window.ethereum.enable();
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ account: accounts });
+    const profileDetail = new web3.eth.Contract(PROFILE_DETAIL_ABI, PROFILE_DETAIL_ADDRESS)
+    this.setState({ profileDetail })
+    const reviewReceivedCount = await profileDetail.methods.reviewReceivedCount().call()
+    const reviewGivenCount = await profileDetail.methods.reviewGivenCount().call()
+
+    this.setState({ reviewGivenCount })
+    for (var i = 1; i <= reviewGivenCount; i++) {
+      const reviewGiven = await profileDetail.methods.reviewGivens(i).call()
+      this.setState({
+        reviewGivens: [...this.state.reviewGivens, reviewGiven]
+      })
+    }
+
+    this.setState({ reviewReceivedCount })
+    for (var i = 1; i <= reviewReceivedCount; i++) {
+      const receivedReview = await profileDetail.methods.receivedReviews(i).call()
+      this.setState({
+        receivedReviews: [...this.state.receivedReviews, receivedReview]
+      })
+    }
+
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+      reviewGivenCount: 0,
+      reviewReceivedCount: 0,
+      reviewGivens: [],
+      receivedReviews: [],
     }
   }
 
