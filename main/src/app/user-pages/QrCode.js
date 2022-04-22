030189e54b8ed12559e5f96f0247ed091849e9eb
phanducanh96 <aphan@uvm.edu>
2022-04-21 21:57:38 -0400

    Got QR Code to show up

--- main/src/app/user-pages/QrCode.js
+++ main/src/app/user-pages/QrCode.js
@@ -1,41 +1,48 @@
-import React, {Component} from 'react';
+import React, { Component, Popup } from 'react';
 import axios from 'axios';
 
 class QrCode extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			visible: false,
 			qrCode: null
 		}
 	}
 
 	render() {
+
+		const qr_img = (this.state.visible ? <img src={this.state.qrCode} /> : null)
+
 		return (
 			<div>
 				<button onClick={() => this.getQrCode()}>Click me for QR</button>
-				{this.state !== null && <div>
-              <img src= {this.state.qrCode}/>
-            </div>
-        }
+
+				<div>
+					{qr_img}
+				</div>
+
 			</div>
 		);
 	}
 
 	getQrCode() {
-		axios.get('/api/users', {responseType: 'blob'})
-		.then((response) => {
-			console.log(response.data)
-			// let img = URL.createObjectURL(response)
-			this.setState({
-				qrCode: response.data
-			});
-		}).catch((error) => {
+		axios.get('/api/users', { responseType: 'blob' })
+			.then((response) => {
+				console.log(response.data)
+				// let img = URL.createObjectURL(response)
+				this.setState({
+					qrCode: URL.createObjectURL(response.data),
+					visible: true
+				});
+
+			}).catch((error) => {
 				if (error.response) {
 					console.log(error.response)
 					console.log(error.response.status)
 					console.log(error.response.headers)
 				}
-		});
+			});
 	}
 }
 
